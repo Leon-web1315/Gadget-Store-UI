e3a2817fa11859994c92302cbf56eb14eb91f7bb
Leon-web1315 <ryotarokajita0324@gmail.com>
2026-05-08 23:57:27 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,9 +5,50 @@ const products = [
     { name: "USB-C Hub", category: "accessory", price: 3000,img:"images/imgHub.jpg"},
 ];
 
+
+let currentCategory = "all";
+let currentKeyword = "";
+
+function applyFilters(){
+    let filtered = [...products];
+
+    //カテゴリ
+    if (currentCategory !== "all") {
+        filtered = filtered.filter(p => p.category === currentCategory);
+    }
+
+    //検索
+    if (currentKeyword !== "") {
+        filtered = filtered.filter(p => 
+            p.name.toLowerCase().includes(currentKeyword)
+        );
+    }
+
+    //並び替え
+    if (currentSort === "low") {
+    filtered.sort((a,b) => a.price - b.price);
+    } else if (currentSort === "high") {
+    filtered.sort((a,b) => b.price - a.price);
+    }
+
+    displayProducts(filtered);
+}
+
+
+
+
 function displayProducts(list) {
     const container = document.getElementById("product-list");
+    const count = document.getElementById("result-count");
     container.innerHTML = "";
+
+    count.textContent = `${list.length}件表示中`;
+
+    if (list.length === 0) {
+        container.innerHTML = "<p>該当する商品がありません</p>";
+        
+        return;
+    }
 
     list.forEach(p => {
         const card = document.createElement("div");
@@ -23,13 +64,6 @@ function displayProducts(list) {
     });
 };
 
-function filterProducts(category) {
-    const filtered = category === "all"
-    ? products
-    : products.filter(p => p.category === category);
-
-    displayProducts(filtered);
-};
 
 const categories = ["all","keyboard","mouse","audio","accessory"];
 
@@ -43,8 +77,10 @@ categories.forEach(cat => {
     };
 
     btn.onclick = () => {
+        currentCategory = cat;
+
         //フィルター処理
-        filterProducts(cat);
+        applyFilters();
 
         //activeリセット
         document.querySelectorAll("#buttons button").forEach(b => {
@@ -59,3 +95,49 @@ categories.forEach(cat => {
 });
 
 displayProducts(products);
+
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("input", () => {
+    currentKeyword = searchInput.value.toLowerCase();
+    applyFilters();
+});
+
+
+
+let currentSort = "default";
+
+const sortButtons = document.querySelectorAll("#sort-low, #sort-high");
+
+document.getElementById("sort-low").onclick = () => {
+
+    if (currentSort === "low"){
+        currentSort = "default";
+
+        sortButtons.forEach(b => b.classList.remove("active"));
+    } else{
+        currentSort = "low";
+
+        sortButtons.forEach(b => b.classList.remove("active")); 
+        document.getElementById("sort-low").classList.add("active");
+    }
+    applyFilters();   
+};
+
+
+
+document.getElementById("sort-high").onclick = () => {
+
+    if (currentSort === "high"){
+        currentSort = "default";
+
+        sortButtons.forEach(b => b.classList.remove("active"));
+    } else{currentSort = "high";
+
+        sortButtons.forEach(b => b.classList.remove("active"));
+        document.getElementById("sort-high").classList.add("active");
+    }
+    applyFilters();
+};
+
